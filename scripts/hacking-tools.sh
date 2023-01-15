fb0612cf18a34e29b0be5c9d47007bd4897a3dd0
nahuelrm <nahuelramosm@gmail.com>
2023-01-15 15:20:08 -0300

    hacking tools installer script

---
+++ scripts/hacking-tools.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+packages="httprobe amass findomain sublist3r subfinder nuclei gospider gowitness jq htmlq assetfinder"
+
+sudo pacman -S $packages
+
+# Other tools 
+
+git clone https://github.com/nahuelrm/crtsh ~/Desktop/stderr/repos/crtsh; cd ~/Desktop/stderr/repos/crtsh; ./install.sh; cd 
+
+git clone https://github.com/nahuelrm/BugBountyNotes ~/Documentos/BugBountyNotes
+
+git clone https://github.com/nahuelrm/recon ~/Documentos/recon
+
+git clone https://github.com/nahuelrm/recon-tools ~/tests/recon-tools
+
+git clone https://github.com/nahuelrm/hacking-toolsrc ~/tests/hacking-toolsrc
+
+sudo pacman -S python-setuptools
+git clone https://github.com/xnl-h4ck3r/xnLinkFinder.git ~/Desktop/stderr/repos/xnLinkFinder; cd ~/Desktop/stderr/repos/xnLinkFinder; sudo python setup.py install; cd
+
+alias xnlinkfinder='python3 /home/stderr/Desktop/stderr/repos/xnLinkFinder/xnLinkFinder.py' >> ~/.zshrc
+
+sudo wget https://raw.githubusercontent.com/nahuelrm/web-screenshot/main/web-screenshot.sh -O /bin/web-screenshot && sudo chmod +x /bin/web-screenshot
+
+go install github.com/nahuelrm/goxy@latest
+
+mkdir ~/.gf 2>/dev/null
+git clone https://github.com/tomnomnom/gf /tmp/gf
+mv /tmp/gf/examples/* ~/.gf
+rm -fr /tmp/gf
+sudo ln -sf ~/.gf /root/.gf
+
+go install github.com/tomnomnom/gf@latest
+
+go install github.com/tomnomnom/anew@latest
