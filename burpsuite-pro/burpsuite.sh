4e51a1bcc93a9d3eeb41befba5c28e0097c147ed
nahuelrm <nahuelramosm@gmail.com>
2023-01-21 14:47:34 -0300

    burp update

--- burpsuite-pro/burpsuite.sh
+++ burpsuite-pro/burpsuite.sh
@@ -2,9 +2,9 @@
 
 # Burpsuite
 
-wget 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.8.2&type=jar' -O Burp_Suite_Pro.jar --quiet --show-progress
+sudo wget 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.8.2&type=jar' -O /bin/Burp_Suite_Pro.jar --quiet --show-progress
 (java -jar keygen.jar) &
 
-sudo echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/Burp_Suite_Pro.jar &" > /bin/burpsuite
+sudo echo "java --illegal-access=permit -Dfile.encoding=utf-8 -javaagent:$(pwd)/loader.jar -noverify -jar /bin/Burp_Suite_Pro.jar &" > /bin/burpsuite
 sudo chmod +x /bin/burpsuite
 /bin/burpsuite & disown
